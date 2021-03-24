641f82eb92573becb43724368bd0f597af2c47ff
Aurore Denis <adeni957@gmail.com>
2021-03-24 17:55:17 +0100

    date au format français TODO : mettre une valeur par défaut

--- src/Form/SortiesType.php
+++ src/Form/SortiesType.php
@@ -8,9 +8,11 @@ use App\Entity\Sortie;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class SortiesType extends AbstractType
 {
@@ -25,9 +27,14 @@ class SortiesType extends AbstractType
                 'choice_label' => 'nom_campus',
                 'multiple'=> false, 'expanded'=> false,
             ])
-            ->add('date_debut')
+            ->add('date_debut', DateType::class, [
+                'label' => 'Date de la sortie',
+                'format' => 'dd-MMMM-yyyy'
+            ])
             ->add('duree')
-            ->add('date_cloture')
+            ->add('date_cloture',DateType::class, [
+        'format' => 'dd-MMMM-yyyy'
+    ])
             ->add('nb_inscriptions_max')
             ->add('description_infos')
             ->add('url_photo')
